b456909737665921b47662aed2a2f55f2761e2d4
ragingtitan <135655078+ragingtitan@users.noreply.github.com>
2023-11-11 14:41:10 +0000

    added and changed bootstap

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,19 +3,48 @@ import './App.css';
 
 function App() {
   return (
-    <div className="navigation-bar">
-      <nav className='navbar'>
-        <ul>
-          <li className='logo'><img src="" alt="" /></li>
-          <li className='home'>Home</li>
-          <li className='register'>Register</li>
-          <li className='Services'>Services</li>
-          <li className='community'>Community</li>
-          <li className='about'>About</li>
-          <li className='contact'>contact</li>
-        </ul>
-      </nav>
+<nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" href="/">About</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle active" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Login/Register
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="/"></a></li>
+            <li><a className="dropdown-item" href="/">Login</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="/">Register</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-disabled="page">Community</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-disabled="page">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-disabled="page">Feedback</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
   );
 }
 
