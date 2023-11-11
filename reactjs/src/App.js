5188c53b8ccda7e649b410f4aa9195dba2c1a3c2
ragingtitan <135655078+ragingtitan@users.noreply.github.com>
2023-11-11 19:21:42 +0530

    Add files via upload

---
+++ reactjs/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="navigation-bar">
+      <nav className='navbar'>
+        <ul>
+          <li className='logo'><img src="" alt="" /></li>
+          <li className='home'>Home</li>
+          <li className='register'>Register</li>
+          <li className='Services'>Services</li>
+          <li className='community'>Community</li>
+          <li className='about'>About</li>
+          <li className='contact'>contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
