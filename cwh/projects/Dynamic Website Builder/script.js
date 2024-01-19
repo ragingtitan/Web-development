2a328b3474534bae581112e12d4ac2a8354e2c46
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-01-19 19:18:42 +0000

    finished project

--- cwh/projects/Dynamic Website Builder/script.js
+++ cwh/projects/Dynamic Website Builder/script.js
@@ -1,45 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function addCard(thumbnail,title,channelName,views,duration,date)
-{
-    let card=`<div class="card">
-    <div class="card-wrapper">
-        <div>
-            <img id="card-img" src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ" alt="">
-            <span id="duration-layover-text"></span>
-        </div>
-        <div class="card-body">
-            <div class="card-title">
-                <h3 id="card-title-text"></h3>
-            </div>
-            <div class="card-info">
-                <div class="channel-name">
-                    <h4 id="channel-name-text" class="fw-normal"></h4>
-                </div>
-                <div class="views">
-                    <h4 id="views-text" class="fw-normal "></h4>
-                </div>
-                <div class="time">
-                    <h4 id="time-text" class="fw-normal"></h4>
+document.addEventListener('DOMContentLoaded',()=>{
+    function addcard(thumbnail,title,views,date,duration,no)
+    {
+        let card=`
+        <div class="card">
+        <div class="no">${no}</div>
+            <div class="card-wrapper">
+                <div>
+                    
+                    <img class="card-img" src="${thumbnail}" alt="">
+                    <div class="duration-layover">
+                        <span class="duration-layover-text">${duration}<span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>`;
-let cardcontainer=document.querySelector('.body-wrapper');
-let lengthtext=document.getElementById('duration-layover-text');
-let titletext=document.getElementById('card-title-text');
-let channelNametext=document.getElementById('channel-name-text');
-let viewstext=document.getElementById('views-text');
-let timetext=document.getElementById('time-text');
-let image=document.getElementById('card-img');
-cardcontainer.insertAdjacentHTML('afterbegin',card);
-lengthtext.innerHTML=duration;
-titletext.innerHTML=title;
-channelNametext.innerHTML=channelName;
-viewstext.innerHTML=views;
-timetext.innerHTML=date;
-image.src=thumbnail;
-}
-addCard(`https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ`,`Installing VS CODE & How Websites Work | Sigma Web Development Course - Tutorial #1`,`CodeWithHarry`,935000,'31:19',`3 months ago`);
-addCard(`https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ`,`Installing VS CODE & How Websites Work | Sigma Web Development Course - Tutorial #1`,`CodeWithHarry`,935000,'31:19',`3 months ago`);
-});
+            <div class="card-body">
+                <div class="card-title">
+                    <h2>${title}</h2>
+                </div>
+                <div class="card-info">
+                    <div class="channel-name">
+                        <h4>CodeWithHarry</h4>
+                    </div>
+                    <div class="views">
+                        <h4>${views} views</h4>
+                    </div>
+                    <div class="time">
+                        <h4>${date}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        let body=document.querySelector('.body-wrapper');
+        body.innerHTML+=card;
+    }
+    
+    addcard('https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ','Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1',954000,'3 months ago','31:20',1)
+    addcard('https://i.ytimg.com/vi/kJEsTjH5mVg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBTa4i6NuryzZhcPo7BkCK1G6PqGg','Your First HTML Website | Sigma Web Development Course  Tutorial - #2',467000,'3 months ago','28:31',2)
+    addcard('https://i.ytimg.com/vi/BGeDBfCIqas/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAjPtvUdoA1O40DUFC8U1NdTYLk1g','Basic Structure of an HTML Website | Sigma Web Development Course  Tutorial - #3',290000,'3 months ago','11:12',3);
+    addcard('https://i.ytimg.com/vi/nXba2-mgn1k/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBEoDwrQoTELgI-X7BTCJYva_ekhg','Heading, Paragraphs and Links | Sigma Web Development Course  Tutorial - #4',250000,'3 months ago','19:34',4);
+    addcard('https://i.ytimg.com/vi/1BsVhumGlNc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBWvC8YQRU7FJg3FqTenAIsD_zwGA','Image, Lists, and Tables in HTML | Sigma Web Development Course  Tutorial - #5',246000,'3 months ago','18:20',5);
+})
