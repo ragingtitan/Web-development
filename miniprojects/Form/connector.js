7aa3149bcfa4b526279cd8b2653db4416b4843ae
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-01-23 05:35:19 +0000

    removed Form

--- miniprojects/Form/connector.js
+++
@@ -1,8 +0,0 @@
-const mysql=require('mysql2');
-let connection=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"28469",
-    database:"form"
-});
-module.exports=connection;
